8e75993f2b2f7b4b4f240c76a9b0d86440889285
Daniel Thompson <dlt0012@auburn.edu>
2017-10-20 20:05:18 -0500

    Reorganized database; added schema for reference.

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE
+IF NOT EXISTS Product (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  quantity INTEGER NOT NULL,
+  price DECIMAL(9,2) NOT NULL,
+  provider TEXT NOT NULL,
+  provider_contact TEXT
+);
+
+CREATE TABLE
+IF NOT EXISTS Order (
+  id INTEGER PRIMARY KEY,
+  total DECIMAL(9,2),
+  date DATE default (date('now'))
+);
+
+CREATE TABLE
+IF NOT EXISTS OrderLine (
+  id INTEGER PRIMARY KEY,
+  order_id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
+  quantity INTEGER NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES Order (id),
+  FOREIGN KEY (product_id) REFERENCES Product (id)
+);
--- db_utils.py
+++ db_utils.py
@@ -1,35 +1,54 @@
 import sqlite3
 
 
+def connect_db():
+    return sqlite3.connect('db/database.db', )
+
+
 def product_table():
-    conn = sqlite3.connect('database.db')
+    conn = connect_db()
     c = conn.cursor()
     c.execute("SELECT name, id, quantity, price, provider, provider_contact "
               "FROM Product ORDER BY id")
     result = c.fetchall()
+    c.close()
+    return result
+
+
+def order_table():
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("SELECT ")
+    result = c.fetchall()
+    c.close()
     return result
 
 
 def delete_product(product_id):
-    conn = sqlite3.connect('database.db')
-    conn.execute("DELETE FROM Product WHERE id = ?", (product_id,))
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("DELETE FROM Product WHERE id = ?", (product_id,))
     conn.commit()
+    c.close()
 
 
 def add_new_product(item_dict):
-    conn = sqlite3.connect('database.db')
+    conn = connect_db()
+    c = conn.cursor()
     conn.execute("INSERT INTO Product"
                  "    (name,quantity,price,provider,provider_contact) "
                  "VALUES (:name,:quantity,:price,:provider,:provider_contact)",
                  dict(item_dict))
     conn.commit()
+    c.close()
 
 
 def get_product(product_id):
-    conn = sqlite3.connect('database.db')
+    conn = connect_db()
     c = conn.cursor()
     c.execute("SELECT * FROM Product WHERE id = ?", (product_id,))
     result = c.fetchall()
+    c.close()
     return result[0]
 
 
@@ -39,7 +58,7 @@ def update_product(product, product_id):
     :param FormsDict product: Product info form passed in from POST.
     :param int product_id: Product.id key used for database lookup.
     """
-    conn = sqlite3.connect('database.db')
+    conn = connect_db()
     conn.execute("UPDATE Product SET name = ? WHERE id = ?",
                  (product["name"], product_id))
     conn.execute("UPDATE Product SET quantity = ? WHERE id = ?",
