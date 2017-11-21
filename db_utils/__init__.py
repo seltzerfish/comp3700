7f0d89a6286c8d7e2e6a160d45ffe47a616d1e71
Daniel Thompson <dlt0012@auburn.edu>
2017-11-20 21:05:17 -0600

    Added Table database access.

--- db_utils/__init__.py
+++ db_utils/__init__.py
@@ -1,53 +1,145 @@
 #!/usr/bin/env python
+from bottle import FormsDict
 import sqlite3
+from typing import Any, ClassVar, Iterable, List, Tuple
 
 
-def connect_db(name):
-    return sqlite3.connect(f'db/{name}')
+def connect_db(name: str) -> sqlite3.Connection:
+    """Connect to an SQLite database.
+
+    :param name: filename of an SQLite database
+    :return: an SQLite database connection
+
+    .. note:: Attaches an sqlite3.Row row factory to the connection to allow
+        indexing of fetched rows.
+    """
+    conn = sqlite3.connect(f'db/{name}')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 class SQLiteDatabase:
+    """An SQLite database controller."""
     def __init__(self, name):
         self.connection = connect_db(name)
 
-    def execute(self, query, params=tuple()):
+    def execute(self, query: str, params: Iterable=()) -> sqlite3.Cursor:
+        """Executes an SQLite query.
+
+        :param query: an SQLite query
+        :param params: a set of query parameters (in order)
+        :return: a cursor over the result of the execution
+        """
         with self.connection as conn:
             return conn.execute(query, params)
 
-    def executemany(self, query, seq_of_params):
+    def executemany(self, query: str,
+                    seq_of_params: Iterable[Iterable]=()) -> sqlite3.Cursor:
+        """Executes an SQLite query multiple times with different parameters.
+
+        Similar to :func:`~dbutils.SQLitedatabase.execute`, but performed over
+        every set of parameters in `seq_of_params`.
+
+        :param query: an SQLite query
+        :param seq_of_params: a sequence of sets of query parameters
+        :return: a cursor over the result of the execution
+        """
         with self.connection as conn:
             return conn.executemany(query, seq_of_params)
 
-    def fetchone(self, query, params=tuple()):
+    def fetchone(self, query: str, params: Iterable=()) -> sqlite3.Row:
+        """Fetches one row from an SQLite query.
+
+        :param query: an SQLite query
+        :param params: a set of query parameters (in order)
+        :return: a single row fetched from an SQLite query
+        """
         c = self.execute(query, params)
         return c.fetchone()
 
-    def fetchall(self, query, params=tuple()):
+    def fetchall(self, query: str, params: Iterable=()) -> List[sqlite3.Row]:
+        """Fetches all rows from an SQLite query.
+
+        :param query: an SQLite query
+        :param params: a set of query parameters (in order)
+        :return: all rows fetched from an SQLite query
+        """
         c = self.execute(query, params)
         return c.fetchall()
 
-    def get_table(self, table):
-        self.fetchall('SELECT * FROM ?',
-                      (table,))
+    def get_table(self, table: str, desc: bool=False) -> List[sqlite3.Row]:
+        """Fetches all rows from a specified table in the database.
 
-    def get_row(self, table, key, value):
-        self.fetchone('SELECT * FROM ? WHERE ? = ?',
-                      (table, key, value))
+        :param table: case-sensitive name of the table
+        :param desc: if `True`, table will be ordered by descending ROWID
+        :return: all rows from specified table in database
+        """
+        query, params = 'SELECT * FROM ?', (table,)
+        if desc:
+            query = 'SELECT * FROM ? ORDER BY ?.ROWID DESC'
+            params = (table, table)
+        return self.fetchall(query, params)
 
-    def add_default_row(self, table):
+    def get_row(self, table: str, key: Tuple[str, Any]) -> sqlite3.Row:
+        """Fetches a row from a specified table in the database.
+
+        :param table: case-sensitive name of the table
+        :param key: `tuple` (name, value) pair for the primary key
+        :return: a row matching the provided key pair
+        """
+        return self.fetchone('SELECT * FROM ? WHERE ? = ?',
+                             (table, *key))
+
+    def add_default_row(self, table: str):
+        """Adds a row with default values into the specified table.
+
+        :param table: case-sensitive name of the table
+        """
         self.execute('INSERT INTO ? DEFAULT VALUES',
                      (table,))
 
-    def update_row(self, table, field, data, key, value):
-        self.execute('UPDATE ? SET ? = ? WHERE ? = ?',
-                     (table, field, data, key, value))
+    def update_row_from_form(self, form: FormsDict, table: str,
+                             key: Tuple[str, Any]):
+        """Updates a row in the specified table using a POST request form.
 
-    def update_row_from_form(self, form, table, key, value):
-        values = [(table, field, data, key, value)
-                  for field, data in form.items()]
-        self.executemany('UPDATE ? SET ? = ? WHERE ? = ?',
-                         values)
+        :param form: POST request form containing table data
+        :param table: case-sensitive table name
+        :param key: `tuple` (name, value) pair for the primary key
+        """
+        values = []
+        for field, data in form.items():
+            values.append((table, field, data, *key))
+        self.executemany('UPDATE ? SET ? = ? WHERE ? = ?', values)
 
-    def delete_row(self, table, column, value):
-        self.execute('DELETE FROM ? WHERE ? = ?',
-                     (table, column, value))
+
+
+class Table:
+    """A base class for database tables."""
+    table_name: ClassVar[str] = str()
+    primary_key: ClassVar[str] = str()
+
+    def __init__(self, db: SQLiteDatabase):
+        self.db = db
+
+    def get(self, rowid: Any) -> sqlite3.Row:
+        return self.db.get_row(self.table_name, (self.primary_key, rowid))
+
+    def get_by(self, key: Tuple[str, Any]):
+        return self.db.get_row(self.table_name, key)
+
+    def update(self, field: Tuple[str, Any], key: Tuple[str, Any]):
+        """Updates a row in the specified table in the database.
+
+        :param field: `tuple` (name, value) pair for the updated field
+        :param key: `tuple` (name, value) pair for the primary key
+        """
+        self.db.execute('UPDATE ? SET ? = ? WHERE ? = ?',
+                        (self.table_name, *field, *key))
+
+    def delete(self, rowid: Any):
+        """Deletes a row from the table.
+
+        :param rowid: `tuple` (name, value) pair for the primary key
+        """
+        self.db.execute('DELETE FROM ? WHERE ? = ?',
+                        (self.table_name, *rowid))
--- db_utils/items.py
+++ db_utils/items.py
@@ -7,28 +7,10 @@ class ItemDatabase(SQLiteDatabase):
         super().__init__('item_database.db')
 
     def get_product(self, product_id):
-        return self.get_row('Product', 'id', product_id)
+        return self.get_row('Product', ('id', product_id))
 
     def get_product_by_name(self, name):
-        return self.get_row('Product', 'name', name)
-
-    def add_new_product(self, item_dict):
-        query = ('INSERT INTO Product '
-                 '    (name, quantity, price, provider, provider_contact) '
-                 'VALUES '
-                 '    (:name, :quantity, :price, :provider, :provider_contact)')
-        self.execute(query, dict(item_dict))
-
-    def update_product(self, product, product_id):
-        """Update a product in the item database.
-
-        :param FormsDict product: Product info form passed in from POST.
-        :param int product_id: Primary key used for database lookup.
-        """
-        self.update_row_from_form(product, 'Product', 'id', product_id)
-
-    def delete_product(self, product_id):
-        self.delete_row('Product', 'id', product_id)
+        return self.get_row('Product', ('name', name))
 
     def product_table(self):
         query = ('SELECT name, id, quantity, price, provider, '
@@ -36,14 +18,38 @@ class ItemDatabase(SQLiteDatabase):
                  'FROM Product ORDER BY id')
         return self.fetchall(query)
 
+    def add_new_product(self, item):
+        query = ('INSERT INTO Product '
+                 '    (name, quantity, price, provider, provider_contact) '
+                 'VALUES '
+                 '    (:name, :quantity, :price, :provider, :provider_contact)')
+        self.execute(query, dict(item))
+
+    def update_product(self, product, product_id):
+        """Update a product in the item database.
+
+        :param FormsDict product: Product info form passed in from POST.
+        :param int product_id: Primary key used for database lookup.
+        """
+        self.update_row_from_form(product, 'Product', ('id', product_id))
+
+    def delete_product(self, product_id):
+        self.delete_row('Product', ('id', product_id))
+
     def get_order(self, order_id):
-        return self.get_row('"Order"', 'id', order_id)
+        return self.get_row('"Order"', ('id', order_id))
+
+    def order_table(self):
+        return self.get_table('"Order"')
 
     def add_new_order(self):
         self.add_default_row('"Order"')
 
-    def order_table(self):
-        return self.get_table('"Order"')
+    def update_order(self, field, order_id):
+        self.update_row('"Order"', field, ('id', order_id))
+
+    def delete_order(self, order_id):
+        self.delete_row('"Order"', ('id', order_id))
 
     def orderline_table(self, order_id):
         query = ('SELECT Product.name, OrderLine.quantity, OrderLine.price,'
@@ -53,26 +59,30 @@ class ItemDatabase(SQLiteDatabase):
                  'WHERE order_id = ? ORDER BY OrderLine.id')
         return self.fetchall(query, (order_id,))
 
-    def add_orderline(self, item, order_id):
-        result = self.get_product_by_name(item['name'])
-
-        if result is None:
-            # Item not found.
-            return False
-
-        product_id = result[0]
-        price = result[3]
-        total = int(item["quantity"]) * price
-
-        # Update order total.
-        result = self.get_order(order_id)
-        new_total = result[0] + total
-        self.update_row('"Order"', 'total', new_total, 'id', order_id)
-
-        # Add order line.
+    def add_orderline(self, order_id, product_id, quantity, price, cost):
         self.execute('INSERT INTO OrderLine'
                      '    (order_id, product_id, quantity, price, cost) '
                      'VALUES (?, ?, ?, ?, ?)',
-                     (order_id, product_id, item["quantity"], price, total))
+                     (order_id, product_id, quantity, price, cost))
+
+    def add_orderline_from_item(self, item, order_id):
+        product_result = self.get_product_by_name(item['name'])
+        order_result = self.get_order(order_id)
+
+        if product_result is None:
+            # Item not found.
+            return False
+
+        product_id = product_result['id']
+        price = product_result['price']
+        quantity = int(item['quantity'])
+        cost = quantity * price
+
+        # Update order total.
+        total = order_result['total'] + cost
+        self.update_row('"Order"', ('total', total), ('id', order_id))
+
+        # Add order line.
+        self.add_orderline(cost, order_id, price, product_id, quantity)
 
         return True
